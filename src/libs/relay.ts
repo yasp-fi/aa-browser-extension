edd6c1fe99ca3c0266eb96f62626095bd0f768dc
George Molchanov <wertusser@gmail.com>
2023-03-29 06:20:32 +0200

    chore: add gnosis sdk

---
+++ src/libs/relay.ts
@@ -0,0 +1,60 @@
+import { GelatoRelayAdapter, RelayTransaction } from '@safe-global/relay-kit';
+import { MetaTransactionData, OperationType } from '@safe-global/safe-core-sdk-types';
+import { BigNumber } from '@ethersproject/bignumber';
+import { argumentIsMissing } from 'src/utils/argument-is-missing';
+import Safe from '@safe-global/safe-core-sdk';
+
+const {
+  GELATO_RELAY_API_KEY: gelatoApiKey = argumentIsMissing('GELATO_RELAY_API_KEY'),
+} = process.env;
+
+export const relayAdapter = new GelatoRelayAdapter(gelatoApiKey!);
+
+// ref: https://docs.safe.global/learn/safe-core/safe-core-account-abstraction-sdk/relay-kit
+export const relayTransfer = async (
+  destination: string,
+  amount: string,
+  safe: Safe,
+  options: {
+    isSponsored: boolean;
+    chainId: number;
+  } = { isSponsored: false, chainId: 0x5 }
+) => {
+  const safeTransactionData: MetaTransactionData = {
+    to: destination,
+    data: '0x',
+    value: amount,
+    operation: OperationType.Call,
+  };
+
+  const safeTransaction = await safe.createTransaction({ safeTransactionData });
+
+  const signedSafeTx = await safe.signTransaction(safeTransaction);
+
+  const encodedTx = safe
+    .getContractManager()
+    .safeContract.encode('execTransaction', [
+      signedSafeTx.data.to,
+      signedSafeTx.data.value,
+      signedSafeTx.data.data,
+      signedSafeTx.data.operation,
+      signedSafeTx.data.safeTxGas,
+      signedSafeTx.data.baseGas,
+      signedSafeTx.data.gasPrice,
+      signedSafeTx.data.gasToken,
+      signedSafeTx.data.refundReceiver,
+      signedSafeTx.encodedSignatures(),
+    ]);
+
+  const relayTransaction: RelayTransaction = {
+    target: safe.getAddress(),
+    encodedTransaction: encodedTx,
+    chainId: options.chainId,
+    options: {
+      gasLimit: BigNumber.from('10000'),
+      isSponsored: options.isSponsored,
+    },
+  };
+
+  return relayAdapter.relayTransaction(relayTransaction);
+};
--- src/libs/web3-auth.ts
+++ src/libs/web3-auth.ts
@@ -1,22 +1,56 @@
-import {Web3AuthNoModal, Web3AuthNoModalOptions} from '@web3auth/no-modal';
-import { CHAIN_NAMESPACES } from '@web3auth/base'
+import { Web3AuthNoModal, Web3AuthNoModalOptions } from '@web3auth/no-modal';
+import { CHAIN_NAMESPACES } from '@web3auth/base';
+import { SafeAuthConfig, SafeAuthKit } from '@safe-global/auth-kit';
 import { argumentIsMissing } from '../utils/argument-is-missing';
+import Web3AuthAdapter from '@safe-global/auth-kit/dist/src/adapters/Web3AuthAdapter';
+// import { Web3Provider } from '@ethersproject/providers';
+// import { ethers } from 'ethers';
+// import EthersAdapter from '@safe-global/safe-ethers-lib';
+// import { SafeFactory } from '@safe-global/safe-core-sdk';
 
-const {
-    WEB3_AUTH_CLIENT_ID: clientId = argumentIsMissing('WEB3_AUTH_CLIENT_ID'),
-} = process.env
+const { WEB3_AUTH_CLIENT_ID: clientId = argumentIsMissing('WEB3_AUTH_CLIENT_ID') } = process.env;
 
 const OPTIONS: Web3AuthNoModalOptions = {
-    chainConfig: {
-        chainNamespace: CHAIN_NAMESPACES.EIP155,
-        chainId: '0x5',
-        rpcTarget: "https://rpc.ankr.com/eth",
-        displayName: "Ethereum Goerli Testnet",
-        blockExplorer: "https://goerli.etherscan.io",
-        ticker: "ETH",
-        tickerName: "Ethereum",
-    },
-    clientId: clientId!,
-}
+  chainConfig: {
+    chainNamespace: CHAIN_NAMESPACES.EIP155,
+    chainId: '0x5',
+    rpcTarget: 'https://rpc.ankr.com/eth',
+    displayName: 'Ethereum Goerli Testnet',
+    blockExplorer: 'https://goerli.etherscan.io',
+    ticker: 'ETH',
+    tickerName: 'Ethereum',
+  },
+  clientId: clientId!,
+};
+
+const SAFE_AUTH_CONFIG: SafeAuthConfig = {
+  chainId: '0x5',
+  authProviderConfig: {
+    rpcTarget: 'https://rpc.ankr.com/eth', // Add your RPC e.g. https://goerli.infura.io/v3/<your project id>
+    clientId: clientId!, // Add your client id. Get it from the Web3Auth dashboard
+    network: 'testnet', // The network to use for the Web3Auth modal. Use 'testnet' while developing and 'mainnet' for production use
+    theme: 'dark', // The theme to use for the Web3Auth modal
+  },
+};
 
 export const web3Auth = new Web3AuthNoModal(OPTIONS);
+
+// ref: https://docs.safe.global/learn/safe-core/safe-core-account-abstraction-sdk/auth-kit
+export const authKit = new SafeAuthKit(
+  new Web3AuthAdapter(SAFE_AUTH_CONFIG.chainId, SAFE_AUTH_CONFIG.authProviderConfig),
+  SAFE_AUTH_CONFIG
+);
+
+// export const getFactory = async () => {
+//   const provider = authKit.getProvider();
+//   if (!provider) {
+//     throw new Error('Please auth');
+//   }
+
+//   const ethAdapter = new EthersAdapter({
+//     ethers: ethers as any,
+//     signerOrProvider: new Web3Provider(provider),
+//   });
+
+//   return SafeFactory.create({ ethAdapter: ethAdapter })
+// };
